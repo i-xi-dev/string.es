439f8f5c7479080c3ab8607ea372809e5b91c5e0
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-03 19:11:59 +0900

    3.4.0

--- src/utf8.ts
+++ src/utf8.ts
@@ -1,25 +1,60 @@
-import { isString } from "./main.ts";
+import { BOM, isString } from "./main.ts";
 
-let _decoder: TextDecoder;
+let _decoderDiscardBOM: TextDecoder;
+let _decoderPreserveBOM: TextDecoder;
 let _encoder: TextEncoder;
 
 export namespace Utf8 {
-  // discard BOM
-  export function decode(input: BufferSource = new Uint8Array(0)): string {
-    if (!_decoder) {
-      _decoder = new TextDecoder("utf-8", { fatal: true, ignoreBOM: false });
+  export type DecodeOptions = {
+    ignoreBOM?: boolean;
+  };
+
+  export function decode(
+    input: BufferSource = new Uint8Array(0),
+    options: DecodeOptions = {},
+  ): string {
+    let _decoder: TextDecoder;
+    if (options?.ignoreBOM === true) {
+      // preserve BOM
+      if (!_decoderPreserveBOM) {
+        _decoderPreserveBOM = new TextDecoder("utf-8", {
+          fatal: true,
+          ignoreBOM: true,
+        });
+      }
+      _decoder = _decoderPreserveBOM;
+    } else {
+      // discard BOM
+      if (!_decoderDiscardBOM) {
+        _decoderDiscardBOM = new TextDecoder("utf-8", {
+          fatal: true,
+          ignoreBOM: false,
+        });
+      }
+      _decoder = _decoderDiscardBOM;
     }
+
     return _decoder.decode(input);
   }
 
+  export type EncodeOptions = {
+    prependBom?: boolean;
+  };
+
   // does not prepend BOM
-  export function encode(input = ""): Uint8Array {
+  export function encode(input = "", options: EncodeOptions = {}): Uint8Array {
     if (isString(input) !== true) {
       throw new TypeError("input");
     }
 
     if (!_encoder) {
       _encoder = new TextEncoder();
+    }
+
+    if (options?.prependBom === true) {
+      if (input.startsWith(BOM) !== true) {
+        return _encoder.encode(BOM + input);
+      }
     }
     return _encoder.encode(input);
   }
