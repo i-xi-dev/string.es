068973e605c3c6f262eb5c5edead7381275d3000
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-04 09:01:10 +0900

    3.4.1

--- mod.ts
+++ mod.ts
@@ -2,6 +2,5 @@ export * from "./src/encoding.ts";
 export * from "./src/isomorphic.ts";
 export * from "./src/main.ts";
 export * from "./src/us_ascii.ts";
-export * from "./src/utf16be.ts";
-export * from "./src/utf16le.ts";
+export * from "./src/utf16.ts";
 export * from "./src/utf8.ts";
